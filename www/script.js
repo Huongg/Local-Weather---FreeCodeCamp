482d1f5220e7539d3e64023e434e463250e5941b
Huongg <32060364+Huongg@users.noreply.github.com>
2018-04-17 21:42:38 +0100

    Toggle celsius to Fah, vice versa

--- www/script.js
+++ www/script.js
@@ -1,6 +1,46 @@
 
-//TODO: use document.ready
 
+$(document).ready(function(){
+	
+	// From C to F
+	function cToF(celsius) {
+	  return (celsius * 9 / 5 + 32).toFixed(2);
+	}
+	// From F to C
+	function fToC(fahrenheit) {
+	  return ((fahrenheit - 32) * 5 / 9).toFixed(2);
+	}
+
+
+
+	$(".btn").click(function(){
+		let tempInCelsius = $(".temp").text();
+		let tempValInCelsius = tempInCelsius.substring(0, tempInCelsius.length -1);
+		let lastWordOfCelsius = tempInCelsius.charAt(tempInCelsius.length-1);
+		// let lastWordOfFah = 
+		let convertedToF = cToF(tempValInCelsius);
+		let convertedToC = fToC(tempValInCelsius);
+
+
+
+		if (lastWordOfCelsius.charCodeAt(0) === 8451){
+			$(".temp").html(`${convertedToF} &#8457`);
+			$(".btn").text("In Degree");
+
+		} else if(lastWordOfCelsius.charCodeAt(0) === 8457){
+			$(".temp").html(`${convertedToC} &#8451`);
+			$(".btn").text("In Dah");
+
+		}
+		
+		
+
+	})
+	
+	
+
+
+});
 
 // if (navigator.geolocation) {
 //   navigator.geolocation.getCurrentPosition(function(position) {
@@ -8,7 +48,10 @@
 //     var long = position.coords.longitude;
 //     getWeather(lat,long);
 //     })
-//   };
+//   }  else {
+//    loc.innerHTML = "Geolocation is not supported by this browser.";
+//  }
+
 
 //Test purposes
 getWeather(35,139);
@@ -19,7 +62,7 @@ getWeather(35,139);
 function getWeather(lat, long) {
 	const root = "https://fcc-weather-api.glitch.me/api/current?";
 	var myReqDynamic = `${root}lat=${lat}&lon=${long}`;
-	var myReqStatic = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
+	//var myReqStatic = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
 
 
 	var reqType = "get";
@@ -39,8 +82,10 @@ function updateDataToUI(cityName, weather, temp, icon){
 
 	//IMplement here
 	$(".city").text(cityName);
-	$(".temp").text(temp);
+	$(".temp").html(`${temp} &#8451`);
 	$(".status").text(weather[0]["description"]);	
 	$(".icon").html(`<img src="${weather[0].icon}" />`);
 }
+
+
 
