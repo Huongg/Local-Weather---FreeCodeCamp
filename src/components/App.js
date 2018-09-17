6405230ca3af4648e47f59f8b55cd87fa4093967
Huongg <32060364+Huongg@users.noreply.github.com>
2018-09-17 14:30:19 +0100

    Added conditional statement in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import SetupMenu from './SetupMenu/SetupMenu.js';
+import MainWeather from './MainWeather/MainWeather.js';
+import AppStore from '../libs/AppStore.js';
 
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			finishedSetUp:undefined
+		}
+	}
+	
+	componentDidMount() {
+	    AppStore.addChangeListener('STORE_CLICK_OK', this.onChange);
+	}
+
+	// Callbacks
+	onChange = () => {
+		this.updateFinishedSetUp();
+  	}
+
+
+  	// Logic
+	updateFinishedSetUp() {
+		this.setState ({
+			finishedSetUp: AppStore.getFinishedSetUp()
+		})
+	}
 	render() {
 	    return (
-		  <SetupMenu />
+	    	<div>
+				{ this.state.finishedSetUp
+				  ? <MainWeather />
+				  : <SetupMenu />
+				}
+			</div>
 	    )
 	}
 }
---
+++ src/components/MainWeather/MainWeather.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MainWeather extends Component {
+
+	render() {
+		return(
+			<div>
+				<p>You're in the main menu</p>
+			</div>
+			
+
+		)
+	}
+
+}
+
+export default MainWeather;
